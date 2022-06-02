9d7821c6ae6894bf3aba39d851a29a1263ae7e50
Ahad Lag <mr.ahad.lag@gmail.com>
2022-06-02 12:31:41 +0430

    complete chenge button in filter todo

--- src/components/todos/todoList/filterTodos.js
+++ src/components/todos/todoList/filterTodos.js
@@ -1,7 +1,9 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { chengeFilter } from './../../../store/slices/todoSlice';
 
 export default function FilterTodos() {
+
+    const filterTodoFlag = useSelector(state => state.todo.filterTodoFlag);
 
     const dispatch = useDispatch();
 
@@ -13,7 +15,7 @@ export default function FilterTodos() {
                 type="button"
                 name='all'
                 onClick={allFilterhandler}
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-sky-700 hover:bg-sky-700 hover:text-white ml-2"
+                className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm ${filterTodoFlag === 'all' ? 'text-white bg-sky-700 hover:bg-sky-700' : 'text-gray-600 bg-sky-100 hover:bg-sky-700'} hover:text-white ml-2`}
             >
                 همه
             </button>
@@ -21,7 +23,7 @@ export default function FilterTodos() {
                 type="button"
                 name='do'
                 onClick={allFilterhandler}
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-gray-600 bg-green-100 hover:bg-green-700 hover:text-white ml-2"
+                className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm ${filterTodoFlag === 'do' ? 'text-white bg-green-700 hover:bg-green-700' : 'text-gray-600 bg-green-100 hover:bg-green-700'} hover:text-white ml-2`}
             >
                 تکمیل شده
             </button>
@@ -29,7 +31,7 @@ export default function FilterTodos() {
                 type="button"
                 name='undo'
                 onClick={allFilterhandler}
-                className="inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm text-gray-600 bg-rose-100 hover:bg-rose-700 hover:text-white"
+                className={`inline-flex items-center px-4 py-2 border border-transparent text-base font-medium rounded-md shadow-sm ${filterTodoFlag === 'undo' ? 'text-white bg-rose-700 hover:bg-rose-700' : 'text-gray-600 bg-rose-100 hover:bg-rose-700'} hover:text-white`}
             >
                 تکمیل نشده
             </button>
