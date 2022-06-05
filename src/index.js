4c48ecb0e2ef27dbb923706d7647d6bc687f2eaf
ahad-lag <mr.ahad.lag@gmail.com>
2022-06-05 20:06:19 +0430

    seve redusers in localStorage whit redux persist

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store , persistor } from './store';
+import { PersistGate } from 'redux-persist/integration/react'
 import './assets/css/index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
   </Provider>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
+import {combineReducers, configureStore} from '@reduxjs/toolkit'
 import todoSlice from './slices/todoSlice';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const rootReducer = combineReducers({
+  todo : todoSlice
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['todo']
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    todo : todoSlice
-  },
+  reducer: persistedReducer,
 })
+
+export const persistor = persistStore(store)
