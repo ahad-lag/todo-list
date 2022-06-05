181c42268914e8128363ebe254bd5658b279c329
ahad-lag <mr.ahad.lag@gmail.com>
2022-06-05 14:19:58 +0430

    create modal folder and create base modal

--- src/components/todos/index.js
+++ src/components/todos/index.js
@@ -1,8 +1,12 @@
 import Header from "./layout/header"
 import InsertTodo from "./InsertTodo/insertTodo"
 import TodoListBox from "./todoList/todoListBox"
+import Modal from "../modal/modal";
+import {useState} from "react";
 
 export default function Todos() {
+
+    const [ showModal , setShowModal ] = useState(true);
 
     return(
         <div className="min-h-full flex flex-col justify-center items-center py-12 sm:px-6 lg:px-8">
@@ -15,6 +19,15 @@ export default function Todos() {
 
             {/* import Todo list box */}
             <TodoListBox />
+
+            {/* import modal */}
+            <Modal
+                showModal={showModal}
+                setShowModal={setShowModal}
+            >
+                <h1>ahad lag</h1>
+            </Modal>
+
         </div>
     )
 }
